0e1c98a54a4db313fd11dfb654124a0f266572f4
eomjengsig <travelereom@naver.com>
2022-10-04 01:58:10 +0900

    as-atrrs

--- src/App.js
+++ src/App.js
@@ -4,27 +4,19 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50%;
-`;
-
-const Text = styled.h1`
-  color: white;
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
     <Father>
-      <Box bgColor="teal">
-        <Text>Hello</Text>
-      </Box>
-      <Circle bgColor="tomato" />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
